6a0ea6aad8da7c8b80761de6d5e1e4384102ca85
Lachezar Georgiev <49004684+glachezar@users.noreply.github.com>
2024-04-23 11:01:27 +0300

    Add project files.

---
+++ Domain/Entities/CoffeeShop.cs
@@ -0,0 +1,12 @@
+﻿namespace Domain.Entities;
+
+public class CoffeeShop
+{
+    public int Id { get; set; }
+
+    public string Name { get; set; } = null!;
+
+    public string OpeningHours { get; set; } = null!;
+
+    public string Address { get; set; } = null!;
+}
---
+++ Infrastructure/Configurations/CoffeeShopEntityConfiguration.cs
@@ -0,0 +1,49 @@
+﻿namespace Infrastructure.Configurations;
+
+using Domain.Entities;
+
+internal class CoffeeShopEntityConfiguration : IEntityTypeConfiguration<CoffeeShop>
+{
+    public void Configure(EntityTypeBuilder<CoffeeShop> builder)
+    {
+        builder
+            .HasData(GenerateCoffeeShops());
+    }
+
+
+    private CoffeeShop[] GenerateCoffeeShops()
+    {
+        ICollection<CoffeeShop> coffeeShops = new HashSet<CoffeeShop>();
+
+        CoffeeShop coffeeShop;
+        coffeeShop = new CoffeeShop
+        {
+            Id = 1,
+            Name = "FirstShop",
+            OpeningHours = "AllTheTime",
+            Address = "It's there"
+        };
+        coffeeShops.Add(coffeeShop);
+
+        coffeeShop = new CoffeeShop
+        {
+            Id = 2,
+            Name = "SecondShop",
+            OpeningHours = "FullTime",
+            Address = "It's somewhere"
+        };
+        coffeeShops.Add(coffeeShop);
+
+        coffeeShop = new CoffeeShop
+        {
+            Id = 3,
+            Name = "ThirdShop",
+            OpeningHours = "PartTime",
+            Address = "It's in the void"
+        };
+        coffeeShops.Add(coffeeShop);
+
+        return coffeeShops.ToArray();
+    }
+}
+}
